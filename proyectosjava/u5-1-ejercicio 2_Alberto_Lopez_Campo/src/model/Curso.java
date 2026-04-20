fc5a4e9500e8aa5b0c1320e1efbe2df8432153a2
Alberto Lopez Campo <albertolc44@educastur.es>
2026-04-20 20:41:16 +0200

    Ejercicio 2

--- proyectosjava/u5-1-ejercicio 2_Alberto_Lopez_Campo/src/model/Curso.java
+++ proyectosjava/u5-1-ejercicio 2_Alberto_Lopez_Campo/src/model/Curso.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Curso {
+    private static final int MAX_ESTUDIANTES = 20;
+    private static final int MAX_ASIGNATURAS = 6;
+    private int[][] notas;
+    public Curso() {
+    }
+    public void inicializar(){
+        notas=new int[MAX_ESTUDIANTES][MAX_ASIGNATURAS];
+        for(int i=0;i<MAX_ESTUDIANTES;i++){
+            notas[i][]=(int) (Math.random() * 10 + 1);
+        }
+    }
 }
